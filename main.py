3cbcca038c14d8fa385b794c2267a8dbed75ffc5
krutojsuper591-del <krutojsuper591@gmail.com>
2026-09-08 17:20:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
 import os
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import threading
 from telebot import TeleBot
 from translators import translate_text
 
-# --- Код фальшивого веб-сервера для бесплатного тарифа Render ---
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"Bot is running!")
-
-def run_web_server():
-    port = int(os.environ.get("PORT", 8000))
-    server = HTTPServer(("0.0.0.0", port), SimpleHTTPRequestHandler)
-    server.serve_forever()
-
-# Запускаем веб-сервер в отдельном потоке
-threading.Thread(target=run_web_server, daemon=True).start()
-# --------------------------------------------------------------
-
-# --- Сам бот-переводчик ---
-BOT_TOKEN = os.environ.get('BOT_TOKEN') 
+# Вставляем токен прямо в код, чтобы не запутаться в настройках Koyeb
+BOT_TOKEN = 8625622751:AAHDsCEX1wSI5mGbb-wa5A1TrUNy_TdgeyA 
 bot = TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
@@ -40,3 +22,4 @@ def translate_message(message):
         bot.reply_to(message, "Произошла ошибка при переводе. Попробуйте еще раз.")
 
 bot.infinity_polling()
+
